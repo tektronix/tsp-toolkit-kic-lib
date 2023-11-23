77501a599787fb183ac616032e0241c4f9dd118f
sjaffery <syed.jaffery@tektronix.com>
2023-11-23 13:48:42 +0530

    Revert "copying tsp-instrument from tsp-toolkit-kic-cli"
    This reverts commit 1acf97b4aa42e198ecdbf969312c8f9262dce9d3.

--- src/test_util/mod.rs
+++
@@ -1,6 +0,0 @@
-pub const SIMPLE_FAKE_BINARY_FW: &[u8] = include_bytes!("./simple_fake_binary_fw.test");
-pub const SIMPLE_FAKE_BINARY_CHUNK0: &[u8] = include_bytes!("./simple_fake_binary_fw.chunk0");
-pub const SIMPLE_FAKE_BINARY_CHUNK1: &[u8] = include_bytes!("./simple_fake_binary_fw.chunk1");
-pub const SIMPLE_FAKE_BINARY_CHUNK2: &[u8] = include_bytes!("./simple_fake_binary_fw.chunk2");
-pub const SIMPLE_FAKE_BINARY_CHUNK3: &[u8] = include_bytes!("./simple_fake_binary_fw.chunk3");
-pub const SIMPLE_FAKE_TEXTUAL_FW: &[u8] = include_bytes!("./simple_fake_textual_fw.test");
